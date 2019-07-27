bd673e72d3daa52de27f5e39c5a6f0069063b890
Arfan Shaikh <36654833+shaikharfan7@users.noreply.github.com>
2019-07-27 21:53:31 +0530

    Delete MyShare.bat

--- MyShare.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title MyShare v1.0 beta  - Project By Arfan Shaikh (15CE13)
-echo MyShare is running ipconfig for determining ip of this computer.
-ipconfig 
-pause
-echo Note your IP / IPv4 Address  - Project By Arfan Shaikh (15CE13):
-cls
-echo Executing program  - Project By Arfan Shaikh (15CE13):
-java myshare
