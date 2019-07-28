76a70d8305a07be3fc3ab669a74bf02f3c6ee3cd
shaikharfan7 <shaikharfan7@gmail.com>
2019-07-28 12:12:43 +0530

    IP address field in CMD

--- MyShare-RUN.bat
+++ MyShare-RUN.bat
@@ -7,5 +7,14 @@ echo Note your IP / IPv4 Address  - Project By Arfan Shaikh (15CE13/shaikharfan7
 cls
 echo Executing program  - Project By Arfan Shaikh (15CE13/shaikharfan7@gmail.com):
 cd src
+echo MyShare is running ipconfig for determining ip of this computer.
+echo I am logged on as %UserName%.
+echo My computer's name is %ComputerName%.
+echo My IP settings are
+ipconfig | find "IPv4 Address" | find /i /v "suffsix"
+
 java myshare
+
+
+
 pause
