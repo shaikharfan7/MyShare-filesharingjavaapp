b61e7ceb51e3ea3b0f980a42e66e0ba17c9d0710
shaikharfan7 <shaikharfan7@gmail.com>
2019-07-28 21:55:36 +0530

    Updated batch file

--- MyShare-RUN.bat
+++ MyShare-RUN.bat
@@ -1,13 +1,10 @@
 @echo off
 title MyShare v1.0 beta  - Project By Arfan Shaikh (15CE13/shaikharfan7@gmail.com)
-echo MyShare is running ipconfig for determining ip of this computer.
-ipconfig 
-pause
-echo Note your IP / IPv4 Address  - Project By Arfan Shaikh (15CE13/shaikharfan7@gmail.com):
 cls
 echo Executing program  - Project By Arfan Shaikh (15CE13/shaikharfan7@gmail.com):
 cd src
 echo MyShare is running ipconfig for determining ip of this computer.
+echo Note your IP / IPv4 Address  - Project By Arfan Shaikh (15CE13/shaikharfan7@gmail.com):
 echo I am logged on as %UserName%.
 echo My computer's name is %ComputerName%.
 echo My IP settings are
